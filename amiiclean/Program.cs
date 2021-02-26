a0e8e6fa0e90746026c6affb87af90b2f93b0132
Daniel <790119+DanTheMan827@users.noreply.github.com>
2021-02-26 15:20:16 -0600

    Initial commit

---
+++ amiiclean/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace amiiclean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                var file = new FileInfo(arg);
+                if (file.Exists)
+                {
+                    var length = file.Length;
+                    if (length == 532 || length == 540 || length == 572)
+                    {
+                        Console.WriteLine(file.FullName);
+
+                        var fileData = File.ReadAllBytes(file.FullName);
+                        new byte[32].CopyTo(fileData, 20);
+                        new byte[392].CopyTo(fileData, 128);
+
+                        var crypted = LibAmiibo.Data.AmiiboTag.FromNtagData(fileData).EncryptWithKeys();
+
+                        using (var outStream = File.Create(file.FullName))
+                        {
+                            outStream.Write(crypted, 0, Convert.ToInt32(length));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
